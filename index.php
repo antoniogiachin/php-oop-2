34dd4ef4e617fae6e488ac07ec9a66241d98c83e
antoniogiachin <giachinantonio@outlook.com>
2022-03-16 21:32:27 +0100

    rimossa parte commentata index

--- index.php
+++ index.php
@@ -8,26 +8,6 @@
     require_once __DIR__ . '/classes/user.php';
 
 
-
-
-    // // cibo
-    // $croccantini= new food('gino', 'gino@gmail.com','crocchets', 23);
-    // $croccantini->setType('food');
-    // $croccantini->setPrice(25);
-    // $croccantini->setIngredients('pane', 'carne', 'pangrattato');
-    // var_dump($croccantini);
-
-    // // accessorio
-    // $cuccia = new accessories('magnolia', 250);
-    // $cuccia->setType('accessories');
-    // $cuccia-> setPrice(25);
-    // var_dump($cuccia);
-
-    // //salute
-    // $antipulci = new health('pulcer', 600, 'antipulci');
-    // $antipulci->setAvailability();
-    // var_dump($antipulci);
-
     //cibo
     $croccantini = new food('gino', 'gino@gmail.com', 'crocchets', 23);
     $croccantini->setType('food');
