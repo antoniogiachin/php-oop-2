436d56836d1fb7a0d12c49d78291e648beb4e41d
antoniogiachin <giachinantonio@outlook.com>
2022-03-16 21:32:06 +0100

    inserita carta di credtio, esercizio completo

--- classes/user.php
+++ classes/user.php
@@ -6,19 +6,76 @@
         protected $mail;
         protected $discount=0;
         private $users = ['gino@gmail.com', 'pino@gmail.com', 'dino@gmail.com'];
+        protected $cardNumber;
+        protected $cardExpiration;
+        protected $cardValid;
 
         //funzione di construct
         public function __construct($username, $mail)
         {
             $this->username = $username;
 
-            $this->mail = $mail;
+            if(stripos($mail, '@') == true && strpos($mail, '.') == true){
+                $this->mail = $mail;
+            } else {
+                $this->mail = 'Mail non fornita!';
+            }
+          
 
             // setta anche il discount
             if(in_array($mail, $this->users)){
                 $this->discount = 20;
             }
 
+        }
+
+        // genera un numero casuale carta di credito
+        public function setCardNumber(){
+
+            $cardNumber='';
+
+            for ($i=0; $i < 15 ; $i++) { 
+                # code...
+                $cardNumber .= rand(0,9);
+            }
+
+            $this->cardNumber = $cardNumber;
+        }
+
+        //genero una data casuale e la imposto come data scadenza carta
+        public function getCardExpiration(){
+            
+
+            //conversione date intervallo in timestamp
+            $fMin = strtotime('01-01-2022');
+            $fMax = strtotime('01-01-2032');
+
+            // genero numero random tra i due intrvalli di data
+            $fVal = mt_rand($fMin, $fMax);
+
+            //converto il valore random in formato data
+            $cardExpiration = date('d-m-Y', $fVal);
+
+            
+            $this->cardExpiration = $cardExpiration;
+            
+        }
+
+        //imposto validita' carta in base a data
+        public function getCardValid(){
+
+            // prendo la data odierna e assieme alla scadenza della carta di credito la rendo con strtotime un timestamp in formato unix e numerico
+            $today = date('d.m.y');
+            $todayInt = strtotime($today);
+
+            //se la scadenza e' inferiore al giorno odierno allora la carta non sara' valida
+            $cardExpirationInt = strtotime($this->cardExpiration);
+
+            if($cardExpirationInt < $todayInt){
+                $this->cardValid = false;
+            } else {
+                $this->getCardValid = true;
+            }
         }
     }
 
--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     require_once __DIR__ . '/classes/user.php';
 
 
+
+
     // // cibo
     // $croccantini= new food('gino', 'gino@gmail.com','crocchets', 23);
     // $croccantini->setType('food');
@@ -40,11 +42,14 @@
     var_dump($cuccia);
 
     //salute
-    $antipulci= new health('marco', 'marco@mail.com', 'pulcer', 600);
+    $antipulci= new health('marco', 'st', 'pulcer', 600);
     $antipulci->setType('health');
     $antipulci->setSubType('antipulci');
     $antipulci->setAvailability();
     $antipulci->getPrice(60);
+    $antipulci->setCardNumber();
+    $antipulci->getCardExpiration();
+    $antipulci->getCardValid();
     var_dump($antipulci);
 
 
